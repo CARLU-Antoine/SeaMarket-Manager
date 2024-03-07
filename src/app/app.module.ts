936b431c7435660030e22dceba5942196a973308
CARLU <antoine.carlu@outlook.fr>
2024-03-07 14:15:53 +0100

    style V1 page login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,35 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from "./app.component"; 
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { AppComponent } from './app.component'; 
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from "@angular/material/card"; 
 import { MatButtonModule } from "@angular/material/button"; 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
-@NgModule({ 
-imports: 
-[BrowserModule, 
-FormsModule, 
-MatToolbarModule,
-MatSidenavModule,
-MatIconModule,
-MatCardModule, 
-MatButtonModule 
-], 
-declarations: [AppComponent,SidenavComponent], 
-bootstrap: [AppComponent], 
-schemas: [CUSTOM_ELEMENTS_SCHEMA]
-}) 
-export class AppModule {}
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    SidenavComponent
+  ],
   imports: [
+    BrowserModule,
+    FormsModule,
+    MatFormFieldModule,
     CommonModule,
-    AppRoutingModule
-  ]
-})
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+}) 
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  standalone: true,
+  imports: [MatFormFieldModule],
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
   constructor(private router: Router) { }
-
+  
   nagivateToSideNav() {
     this.router.navigate(['/sidenav']);
   }
