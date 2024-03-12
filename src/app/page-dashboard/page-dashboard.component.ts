065ff3dce43f3bb91d1a9c28c0adc0ecd628c063
VivienS5 <sobra.vivien@gmail.com>
2024-03-12 10:30:09 +0100

    forme du dashboard finis, manque des composants à mettre

--- src/app/page-dashboard/page-dashboard.component.ts
+++ src/app/page-dashboard/page-dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { SidenavComponent } from '../sidenav/sidenav.component';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
 import { CountUserComponent } from './count-user/count-user.component';
-
+import { CountMoneyComponent } from './count-money/count-money.component';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { MatCard } from '@angular/material/card';
 @Component({
@@ -14,10 +14,12 @@ import { MatCard } from '@angular/material/card';
     CountUserComponent,
     MatCard,
     MatGridListModule,
+    CountMoneyComponent,
   ],
   templateUrl: './page-dashboard.component.html',
   styleUrl: './page-dashboard.component.css'
 })
 export class PageDashboardComponent {
   numberOfUsers = 5; //recuperer nombre de user ici 
+  numberOfMoney = 12345; //recuperer nombre de revenue ici 
 }
