072cdcaf588dd319b56f986fa7fec526a8a7708e
VivienS5 <sobra.vivien@gmail.com>
2024-03-13 16:00:38 +0100

    ajout bouton conffeti

--- src/app/page-dashboard/page-dashboard.component.ts
+++ src/app/page-dashboard/page-dashboard.component.ts
@@ -6,6 +6,8 @@ import { CountMoneyComponent } from './count-money/count-money.component';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { MatCard } from '@angular/material/card';
 import { SellsChartComponent } from './sells-chart/sells-chart.component';
+
+import confetti from 'canvas-confetti';
 @Component({
   selector: 'app-page-dashboard',
   standalone: true,
@@ -17,6 +19,7 @@ import { SellsChartComponent } from './sells-chart/sells-chart.component';
     MatGridListModule,
     CountMoneyComponent,
     SellsChartComponent,
+    
   ],
   templateUrl: './page-dashboard.component.html',
   styleUrl: './page-dashboard.component.css'
@@ -24,4 +27,17 @@ import { SellsChartComponent } from './sells-chart/sells-chart.component';
 export class PageDashboardComponent {
   numberOfUsers = 5; //recuperer nombre de user ici 
   numberOfMoney = 12345; //recuperer nombre de revenue ici 
+
+  celebrate() {
+  const duration = 3000;
+
+  confetti({
+    particleCount: 150,
+    spread: 180,
+    origin: { y: 0.6 },
+    colors: ['#FF4500', '#008080', '#FFD700'],
+  });
+
+  setTimeout(() => confetti.reset(), duration);
+}
 }
