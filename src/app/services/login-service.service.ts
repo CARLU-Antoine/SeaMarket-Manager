349383d92bc400d722ab30b5eafd01cebaf6493c
CARLU <antoine.carlu@outlook.fr>
2024-03-16 10:31:24 +0100

    Ajout connexion serveur + vérfication connexion

--- src/app/services/login-service.service.ts
+++ src/app/services/login-service.service.ts
@@ -6,8 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class LoginService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // Méthode pour authentifier l'utilisateur et obtenir le token
   login(email: string, password: string) {
@@ -19,4 +18,16 @@ export class LoginService {
     return this.http.post<any>('http://127.0.0.1:8000/login/refreshtoken/', { refresh: refreshToken });
   }
 
+  // Méthode pour vérifier si l'utilisateur est connecté
+  isLoggedIn(): boolean {
+    const accessToken = localStorage.getItem('accessToken');
+    return !!accessToken; // Retourne true si le token est présent, false sinon
+  }
+    
+  // Méthode pour se déconnecter de l'application
+    logout(): void {
+      // Supprimer les jetons d'authentification du stockage local
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('refreshToken');
+    }
 }
